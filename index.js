dc5c4f17b0aec6f1d54508787a91ea65594cf92e
nitish mahawar <nitishmahawar112@gmail.com>
2022-12-05 09:11:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+let string = "";
+let buttons = document.querySelectorAll('button')
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.slice(0, -1);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = ''
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == '+') {
+            if (string == '') {
+            } else if (string[string.length - 1] == '+' || string[string.length - 1] == '-' || string[string.length - 1] == '*' || string[string.length - 1] == '/' || string[string.length - 1] == '%') {
+
+            }
+            else {
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        }
+        else if (e.target.innerHTML == '-') {
+            if (string == '') {
+            } else if (string[string.length - 1] == '+' || string[string.length - 1] == '-' || string[string.length - 1] == '*' || string[string.length - 1] == '/' || string[string.length - 1] == '%') {
+
+            }
+            else {
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        }
+        else if (e.target.innerHTML == '*') {
+            if (string == '') {
+            } else if (string[string.length - 1] == '+' || string[string.length - 1] == '-' || string[string.length - 1] == '*' || string[string.length - 1] == '/' || string[string.length - 1] == '%') {
+
+            }
+            else {
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        }
+        else if (e.target.innerHTML == '/') {
+            if (string == '') {
+            } else if (string[string.length - 1] == '+' || string[string.length - 1] == '-' || string[string.length - 1] == '*' || string[string.length - 1] == '/' || string[string.length - 1] == '%') {
+
+            }
+            else {
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        }
+        else if (e.target.innerHTML == '%') {
+            if (string == '') {
+            } else if (string[string.length - 1] == '+' || string[string.length - 1] == '-' || string[string.length - 1] == '*' || string[string.length - 1] == '/' || string[string.length - 1] == '%') {
+
+            }
+            else {
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        }
+        else if (e.target.innerHTML == '+/-') {
+
+        }
+        else {
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+    })
+})
